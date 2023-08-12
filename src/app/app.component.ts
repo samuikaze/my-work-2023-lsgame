59446121e38efdc1802b8fa670cd1c23a6c6c1b2
SamuiKaze <rr5145@gmail.com>
2023-08-13 02:31:56 +0800

    feat: 調整 layouts 資料夾名稱
    調整項目:
      1. 調整 layouts 資料夾名稱。

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { FooterComponent } from './layouts/Base/footer/footer.component';
-import { BodyComponent } from './layouts/Base/body/body.component';
+import { FooterComponent } from './layouts/base/footer/footer.component';
+import { BodyComponent } from './layouts/base/body/body.component';
 import { NavigatorComponent } from './components/navigator/navigator.component';
-import { HeaderComponent } from './layouts/Base/header/header.component';
+import { HeaderComponent } from './layouts/base/header/header.component';
 import 'bootstrap/dist/js/bootstrap.bundle.js'
 
 @Component({
